dfcb71bc0890c81e4dab7198c7499e224f8bb0c1
John Boutros <john.g.boutros@gmail.com>
2017-11-24 23:15:09 +0200

    JB - Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug resolved!

--- Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -39,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private TextView mLifecycleDisplay;
 
-    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    // (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    private static List<String> mLifecycleCallbacks = new ArrayList<String>();
 
     /**
      * Called when the activity is first created. This is where you should do all of your normal
@@ -55,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mLifecycleDisplay = (TextView) findViewById(R.id.tv_lifecycle_events_display);
+        String callbacksString = "";
 
         /*
          * If savedInstanceState is not null, that means our Activity is not being started for the
@@ -67,13 +72,20 @@ public class MainActivity extends AppCompatActivity {
             if (savedInstanceState.containsKey(LIFECYCLE_CALLBACKS_TEXT_KEY)) {
                 String allPreviousLifecycleCallbacks = savedInstanceState
                         .getString(LIFECYCLE_CALLBACKS_TEXT_KEY);
-                mLifecycleDisplay.setText(allPreviousLifecycleCallbacks);
+//                mLifecycleDisplay.setText(allPreviousLifecycleCallbacks);
+                callbacksString = allPreviousLifecycleCallbacks;
             }
         }
 
-        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
+        // (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
+        for (String callback: mLifecycleCallbacks) {
+            callbacksString += callback + "\n";
+        }
 
-        // TODO (5) Clear mLifecycleCallbacks after iterating through it
+        mLifecycleDisplay.setText(callbacksString);
+
+        // (5) Clear mLifecycleCallbacks after iterating through it
+        mLifecycleCallbacks.clear();
 
         logAndAppend(ON_CREATE);
     }
@@ -133,7 +145,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
 
-        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
+        // (2) Add the ON_STOP String to the front of mLifecycleCallbacks
+        mLifecycleCallbacks.add(ON_STOP);
 
         logAndAppend(ON_STOP);
     }
@@ -160,7 +173,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
+        // (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
+        mLifecycleCallbacks.add(ON_DESTROY);
 
         logAndAppend(ON_DESTROY);
     }
