092a5ab485aaee508f44ebb0aa5461972c6a0001
John Boutros <john.g.boutros@gmail.com>
2017-12-01 21:37:45 +0200

    JB - Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints resolved!

--- Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -25,11 +25,12 @@ import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
+import android.text.TextUtils;
 import android.widget.Toast;
 
-// TODO (1) Implement OnPreferenceChangeListener
+// (1) Implement OnPreferenceChangeListener
 public class SettingsFragment extends PreferenceFragmentCompat implements
-        OnSharedPreferenceChangeListener {
+        OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
@@ -40,7 +41,6 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
         SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
         PreferenceScreen prefScreen = getPreferenceScreen();
         int count = prefScreen.getPreferenceCount();
-
         // Go through all of the preferences, and set up their preference summary.
         for (int i = 0; i < count; i++) {
             Preference p = prefScreen.getPreference(i);
@@ -51,7 +51,11 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
                 setPreferenceSummary(p, value);
             }
         }
-        // TODO (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
+        // (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
+        Preference sizePreference = findPreference(getString(R.string.pref_size_key));
+        if (sizePreference != null) {
+            sizePreference.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
@@ -88,10 +92,48 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
         }
     }
 
-    // TODO (2) Override onPreferenceChange. This method should try to convert the new preference value
-    // to a float; if it cannot, show a helpful error message and return false. If it can be converted
-    // to a float check that that float is between 0 (exclusive) and 3 (inclusive). If it isn't, show
-    // an error message and return false. If it is a valid number, return true.
+    /**
+     * Called when a Preference has been changed by the user. This is
+     * called before the state of the Preference is about to be updated and
+     * before the state is persisted.
+     *
+     * @param preference The changed Preference.
+     * @param newValue   The new value of the Preference.
+     * @return True to update the state of the Preference with the new value.
+     */
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+        // (2) Override onPreferenceChange. This method should try to convert the new preference value
+        // to a float; if it cannot, show a helpful error message and return false. If it can be converted
+        // to a float check that that float is between 0 (exclusive) and 3 (inclusive). If it isn't, show
+        // an error message and return false. If it is a valid number, return true.
+
+        if (preference.getKey().equals(getString(R.string.pref_size_key))) {
+            if (newValue == null || !(newValue instanceof String)) {
+                showError("Please input a valid size!");
+                return false;
+            }
+
+            try {
+                float floatValue = Float.parseFloat(newValue.toString());
+                if (floatValue <= 0 || floatValue > 3) {
+                    showError("Size must be > zero and < or = 3!");
+                    return false;
+                }
+
+            } catch (NumberFormatException e) {
+                showError("Please input a valid number!");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void showError(String message) {
+        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
