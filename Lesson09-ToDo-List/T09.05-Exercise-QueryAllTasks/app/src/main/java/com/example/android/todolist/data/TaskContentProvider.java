b25cb5e0a1dd4dfdedca907adee9fbcfca8e32ae
John Boutros <john.g.boutros@gmail.com>
2017-12-17 04:22:48 +0200

    JB - Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks resolved!

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -120,15 +120,37 @@ public class TaskContentProvider extends ContentProvider {
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
 
-        // TODO (1) Get access to underlying database (read-only for query)
+        // (1) Get access to underlying database (read-only for query)
+        SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
 
-        // TODO (2) Write URI match code and set a variable to return a Cursor
+        // (2) Write URI match code and set a variable to return a Cursor
+        Cursor cursor = null;
+        int match = sUriMatcher.match(uri);
 
-        // TODO (3) Query for the tasks directory and write a default case
+        // (3) Query for the tasks directory and write a default case
+        switch (match) {
+            case TASKS:
+                // QUERY
+                cursor = db.query(TaskContract.TaskEntry.TABLE_NAME,
+                                            projection,
+                                            selection,
+                                            selectionArgs,
+                                            null,
+                                            null,
+                                            sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri.toString());
+        }
 
-        // TODO (4) Set a notification URI on the Cursor and return that Cursor
+        // (4) Set a notification URI on the Cursor and return that Cursor
+        if (cursor != null) {
+            cursor.setNotificationUri(getContext().getContentResolver(),uri);
+        }
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        return cursor;
+
+//        throw new UnsupportedOperationException("Not yet implemented");
     }
 
 
